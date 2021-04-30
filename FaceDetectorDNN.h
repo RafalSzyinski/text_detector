7964ccc5cb559ce383ffe644da95098b1891f0aa
RafalSzyinski <rafikop.pl@gmail.com>
2021-04-30 23:55:34 +0200

    FaceDetectorDNN

---
+++ FaceDetectorDNN.h
@@ -0,0 +1,27 @@
+//
+// Created by rafal on 30.04.2021.
+//
+
+#ifndef FACEDETECTOR_FACEDETECTORDNN_H
+#define FACEDETECTOR_FACEDETECTORDNN_H
+
+#include <vector>
+
+#include "opencv2/dnn.hpp"
+
+class FaceDetectorDNN
+{
+private:
+    cv::dnn::Net network;
+    const int width;
+    const int height;
+    const double scale;
+    const cv::Scalar mean_value;
+    const float threshold;
+public:
+    FaceDetectorDNN();
+    std::vector<cv::Rect> detectFace(const cv::Mat& frame);
+};
+
+
+#endif //FACEDETECTOR_FACEDETECTORDNN_H
